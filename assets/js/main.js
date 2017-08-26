df87e86d9f81ccf955e0da66243fedb0dc999d13
Melissa <melissa.pacheco.martinez@gmail.com>
2017-08-26 13:17:49 -0300

    cambios portafolio, falta img fondo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 var myNav = document.getElementById('mynav');
 window.onscroll = function () { 
     "use strict";
-    if (document.body.scrollTop >= 200 ) {
+    if (document.body.scrollTop >= 80 ) {
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
     } 
@@ -10,3 +10,16 @@ window.onscroll = function () {
         myNav.classList.remove("nav-colored");
     }
 };
+
+$(window).scroll(function(){
+  
+  var wintop = $(this).scrollTop(); 
+  
+  $('.section').each(function(){
+    if(wintop > $(this).offset().top - ($(window).height() / 1.9)){
+      $(this).addClass('animate');
+      $(this).find('p').addClass('animate');
+    }
+  });
+
+});
